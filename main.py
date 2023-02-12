b98a8822cf8ddc823085d4a20868240f03929c52
Michael Eshun <mykie.eshun@gmail.com>
2023-02-12 14:12:10 +0000

    Complete Rock, Paper, Scissors game

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import random
 def get_choices():
     options = ['rock', 'scissors', 'paper']
 
-    player_choice = input('Enter a choice [rock, paper, scissors]\n')
-    computer_choice = random.choice(options)
+    player_choice: str = input('Enter a choice [rock, paper, scissors]\n')
+    computer_choice: str = random.choice(options)
 
-    selected_choices = {
+    selected_choices: dict = {
         'player': player_choice,
         'computer': computer_choice
     }
@@ -15,5 +15,28 @@ def get_choices():
     return selected_choices
 
 
+def check_win(player: str, computer: str):
+    if player == computer:
+        return 'It\'s a tie!'
+    elif player == 'rock':
+        if computer == 'scissors':
+            print(f'{player.capitalize()} smashes {computer.capitalize()}. Player wins!')
+        else:
+            print(f'{computer.capitalize()} covers {player.capitalize()}. Player lose!')
+    elif player == 'paper':
+        if computer == 'scissors':
+            print(f'{computer.capitalize()} cuts {player.capitalize()}. Player lose!')
+        else:
+            print(f'{player.capitalize()} covers {computer.capitalize()}. Player wins!')
+    elif player == 'scissors':
+        if computer == 'paper':
+            print(f'{player.capitalize()} cuts {computer.capitalize()}. Player wins!')
+        else:
+            print(f'{computer.capitalize()} smashes {player.capitalize()}. Player lose!')
+
+
 choices: dict = get_choices()
-print(choices)
+player: str = choices['player']
+computer: str = choices['computer']
+
+check_win(player, computer)
